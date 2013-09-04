db2ca3d13e6746291ee011acdc6d9386c5b588cd
Ramdhani <Anonymousramdhani@gmail.com>
2013-09-04 20:45:11 +0700

    php 1

---
+++ about.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<title> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp About</title>
+<link rel="Shortcut icon" href="http://localhost/biodata/icon.ico"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<link href="halaman style.css" type="text/css" rel="stylesheet"></link>
+<link href="halaman menu.css" type="text/css" rel="stylesheet"></link>
+</head>
+<body background="http://localhost/tugas/2.jpg">
+<div class="frame_halaman">
+<div class="header">
+</div>
+<div id="menu">
+
+<ul>
+<li><a href="index.php">Home </a></li>
+<li><a href="profil.php">Profil</a></li>
+<li><a href="guest_book.php">Guest Book</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="tampildata.php">Lihat Data</a></li>
+</ul>
+</div>
+
+<form name="index" action="http://localhost/tugas/index.php" method="POST">
+<table class="halaman" border="0">
+<tr>
+<td class="kiri">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center> My Name Is Ramdhani</center>
+<br>
+<br>
+<br>
+<center> Hacker AD 101 </center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>^_^      ^_^      ^_^</center>
+<br>
+<br>
+<br>
+<br>
+</td>
+<td class="kanan">
+<div id="menu_titel">
+<div id="set_menu_titel">
+Last Posting
+</div>
+- <a href="https://www.facebook.com/dhani.RPL.3">My Facebook</a><br>
+- <a href="https://www.twitter.com/">My Twitter</a><br>
+- <a href="https://www.Anonymousramdhani.blogspot.com">My Blogger</a><br>
+</td>
+</tr>
+</table>
+<div class="footer">
+<center> © 2013 Ramdhani Design Corp. All right reserved.<br>
+Ramdhani
+</div>
+
+</body>
+</html>
